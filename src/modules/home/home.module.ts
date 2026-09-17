1d512bca86c51fa0b27d63e1d1577581b305381c
QIUZAIYOU <qian@asifadeaway.com>
2026-09-17 16:14:54 +0800

    chore: bump to 3.35.3

--- src/modules/home/home.module.ts
+++ src/modules/home/home.module.ts
@@ -26,7 +26,7 @@ interface HomeModuleContext {
 }
 export default {
     name: 'home',
-    version: '3.35.2',
+    version: '3.35.3',
     ...homeHistoryFeatures,
     ...homePaidMarkFeatures,
     async install (this: HomeModuleContext): Promise<void> {
--- src/modules/video/video.module.ts
+++ src/modules/video/video.module.ts
@@ -106,7 +106,7 @@ interface VideoModuleContext {
 }
 export default {
     name: 'video',
-    version: '3.35.2',
+    version: '3.35.3',
     async install (this: VideoModuleContext): Promise<void> {
         this._cleanup = []
         this._modeObservers = []
